9e993bd92b40283b58957e166392ffd0f9a7ce7e
Darshan M <darshanmukunda098@gmail.com>
2023-01-25 14:23:42 +0530

    multiply or divide by 2

--- src/multianddivby2.java
+++ src/multianddivby2.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class multianddivby2 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Multiply with 2 = "+ (n << 1));
+        System.out.println("Divide by 2 = "+ (n >> 1));
+    }
 }
