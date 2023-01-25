1170dbe403691a05a31edaba480d3c5725211e19
Darshan M <darshanmukunda098@gmail.com>
2023-01-25 14:10:55 +0530

    first commit

---
+++ src/checkevenorodd.java
@@ -0,0 +1,5 @@
+public class checkevenorodd {
+    public static void main(String[] args) {
+        System.out.println(0 == (Integer.parseInt(args[0]) & 1));
+    }
+}
---
+++ src/multianddivby2.java
@@ -0,0 +1,2 @@
+public class multianddivby2 {
+}
