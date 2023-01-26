dcd5010ee2c4ad80a2a2dd00c29b7565562c860c
Darshan M <darshanmukunda098@gmail.com>
2023-01-26 22:30:29 +0530

    and

--- src/ArrayListExample4.java
+++ src/ArrayListExample4.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ArrayListExample4 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample4 {
+    public static void main(String[] args){
+        String[] arr = {"a","b","c","d"};
+        ArrayList<String> al = new ArrayList<>(Arrays.asList(arr));
+        al.addAll(List.of(arr));
+        al.add("a");
+        al.add("b");
+        al.add("c");
+        al.add("d");
+        System.out.println(al);
+        System.out.println(al.size());
+    }
 }
--- src/ArrayListLoopExample3.java
+++ src/ArrayListLoopExample3.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ArrayListLoopExample3 {
+import java.util.ArrayList;
+
+public class ArrayListLoopExample3 {
+    public static void main(String[] args){
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(3);
+        al.add(17);
+        al.add(6);
+        al.add(9);
+        al.add(7);
+
+        if(al.contains(7)){
+            System.out.println("7 was found on the list");
+        }else {
+            System.out.println("7 was not found on the list");
+        }
+    }
 }
