5b6f96e1c3915b79b2518c1dbe12f141529f92d9
Darshan M <darshanmukunda098@gmail.com>
2023-01-26 22:33:44 +0530

    Revert "and"
    This reverts commit dcd5010ee2c4ad80a2a2dd00c29b7565562c860c.

--- src/ArrayListLoopExample2.java
+++ src/ArrayListLoopExample2.java
@@ -1,19 +1,2 @@
-import java.util.ArrayList;
-
-public class ArrayListLoopExample2 {
-    public static void main(String[] args){
-        ArrayList<String> cities = new ArrayList<>();
-        cities.add("Dallas");
-        cities.add("Boston");
-        cities.add("San jose");
-        cities.add("Chicago");
-
-        String citinames[] = cities.toArray(new String[cities.size()]);
-
-        for(String str: citinames){
-            System.out.println(str);
-        }
-
-
-    }
+package PACKAGE_NAME;public class ArrayListLoopExample2 {
 }
--- src/ArrayListLoopExample3.java
+++ src/ArrayListLoopExample3.java
@@ -1,18 +1,2 @@
-import java.util.ArrayList;
-
-public class ArrayListLoopExample3 {
-    public static void main(String[] args){
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        al.add(3);
-        al.add(17);
-        al.add(6);
-        al.add(9);
-        al.add(7);
-
-        if(al.contains(7)){
-            System.out.println("7 was found on the list");
-        }else {
-            System.out.println("7 was not found on the list");
-        }
-    }
+package PACKAGE_NAME;public class ArrayListLoopExample3 {
 }
