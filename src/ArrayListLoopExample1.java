8385c0fce94f38f23857dd68b10fc8ffc6bdcbe7
Darshan M <darshanmukunda098@gmail.com>
2023-01-26 21:29:15 +0530

    arrayList1

---
+++ src/ArrayListLoopExample1.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListLoopExample1 {
+    public static void main(String[] args){
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(3);
+        al.add(17);
+        al.add(6);
+        al.add(9);
+        al.add(7);
+        System.out.println("Using Advanced Loop ");
+        System.out.println(al);
+        for(Integer num : al){
+            System.out.println(num);
+        }
+
+    }
+}
