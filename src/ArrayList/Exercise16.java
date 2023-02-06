126645d65470b2f57a3f766628dbf55ac1fdc264
Darshan M <darshanmukunda098@gmail.com>
2023-02-06 17:10:04 +0530

    Exercise 16

---
+++ src/ArrayList/Exercise16.java
@@ -0,0 +1,16 @@
+package ArrayList;
+import java.util.*;
+public class Exercise16 {
+    public static void main(String[] args){
+        List<String> li = new ArrayList<>();
+        li.add("Red");
+        li.add("Green");
+        li.add("Black");
+        li.add("White");
+        li.add("Pink");
+        System.out.println("Original array "+li);
+        ArrayList<String> newli = new ArrayList<>(li);
+        System.out.println("Cloned array");
+        System.out.println(newli);
+    }
+}
