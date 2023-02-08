64b901bc7654de5e47da6bc36eef6b698a7b9dd3
Darshan M <darshanmukunda098@gmail.com>
2023-02-08 15:16:41 +0530

    Excercise 8

---
+++ src/HashMap/Exercise8.java
@@ -0,0 +1,31 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class Exercise8 {
+    public static void main(String[] args){
+        HashMap< String, Integer > hash_map = new HashMap < String, Integer > ();
+        hash_map.put("Red", 1);
+        hash_map.put("Green", 2);
+        hash_map.put("Black", 3);
+        hash_map.put("White", 4);
+        hash_map.put("Blue", 5);
+        // print the map
+        System.out.println("The Original map: " + hash_map);
+        System.out.println("1. Is key 'Green' exists?");
+        if (hash_map.containsValue(2)) {
+            //key exists
+            System.out.println("yes! - " );
+        } else {
+            //key does not exists
+            System.out.println("no!");
+        }
+
+        System.out.println("\n2. Is key 'orange' exists?");
+        if (hash_map.containsKey("orange")) {
+            System.out.println("yes! - " );
+        } else {
+            System.out.println("no!");
+        }
+    }
+}
