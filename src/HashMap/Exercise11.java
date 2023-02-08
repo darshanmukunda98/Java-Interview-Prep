cd4ddee94f1b3aad42012fb0dff8ccb04953eaf5
Darshan M <darshanmukunda098@gmail.com>
2023-02-08 15:25:39 +0530

    Excercise 11

---
+++ src/HashMap/Exercise11.java
@@ -0,0 +1,17 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise11 {
+    public static void main(String[] args){
+        Map< Integer, String > map = new HashMap<>();
+        map.put(1, "Red");
+        map.put(2, "Green");
+        map.put(3, "Black");
+        map.put(4, "White");
+        map.put(5, "Blue");
+        System.out.println(map.keySet());
+
+    }
+}
