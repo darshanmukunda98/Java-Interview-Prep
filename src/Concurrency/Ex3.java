3d33af1e380a36ee096501caa45e16bfeb90c46e
Darshan M <darshanmukunda098@gmail.com>
2023-02-11 21:49:38 +0530

    Using Threads in Java

--- src/Concurrency/Ex3.java
+++ src/Concurrency/Ex3.java
@@ -2,15 +2,15 @@ package Concurrency;
 
 public class Ex3 {
     public static void main(String args[]) throws InterruptedException {
-        String importantInfo[] = {
+        String[] importantInfo = {
                 "Mares eat oats",
                 "Does eat oats",
                 "Little lambs eat ivy",
                 "A kid will eat ivy too"
         };
-        for(int i = 0;i < importantInfo.length;i++){
+        for (String s : importantInfo) {
             Thread.sleep(4000);
-            System.out.println(importantInfo[i]);
+            System.out.println(s);
         }
     }
 }
