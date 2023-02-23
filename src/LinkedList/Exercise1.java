27e26c64ef73e67a5db4c3cd63318c610d7ed5f5
Darshan M <darshanmukunda098@gmail.com>
2023-02-23 12:37:04 +0530

    Exercise1

---
+++ src/LinkedList/Exercise1.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        LinkedList<String> li = new LinkedList<>();
+        li.add("Red");
+        li.add("Green");
+        li.add("Black");
+        li.add("White");
+        System.out.println(li);
+    }
+}
