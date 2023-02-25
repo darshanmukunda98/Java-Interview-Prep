3adb5d436203a9e7d17c65ae30db844a0c32e375
Darshan M <darshanmukunda098@gmail.com>
2023-02-25 08:40:04 +0530

    Exercise4

---
+++ src/PriorityQueue/Exercise4.java
@@ -0,0 +1,16 @@
+package PriorityQueue;
+
+import java.util.PriorityQueue;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        PriorityQueue<String> pq1 = new PriorityQueue<>();
+        pq1.add("Red");
+        pq1.add("Green");
+        pq1.add("Black");
+        pq1.add("White");
+        System.out.println(pq1);
+        pq1.offer("Blue");
+        System.out.println(pq1);
+    }
+}
