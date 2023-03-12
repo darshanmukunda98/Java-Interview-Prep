195a6c288b3c81d7f3e0b8486a53ed55503af1af
Darshan M <darshanmukunda098@gmail.com>
2023-03-12 12:15:53 +0530

    CourierDAO and Courier

--- src/FlipkartInterview/Courier.java
+++ src/FlipkartInterview/Courier.java
@@ -6,6 +6,63 @@ public class Courier {
     private String receiverName;
     private int sourcePincode;
     private int destinationPincode;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public int getSourcePincode() {
+        return sourcePincode;
+    }
+
+    public void setSourcePincode(int sourcePincode) {
+        this.sourcePincode = sourcePincode;
+    }
+
+    public int getDestinationPincode() {
+        return destinationPincode;
+    }
+
+    public void setDestinationPincode(int destinationPincode) {
+        this.destinationPincode = destinationPincode;
+    }
+
+    public CourierType getCourierType() {
+        return courierType;
+    }
+
+    public void setCourierType(CourierType courierType) {
+        this.courierType = courierType;
+    }
+
+    public CourierStatus getCourierStatus() {
+        return courierStatus;
+    }
+
+    public void setCourierStatus(CourierStatus courierStatus) {
+        this.courierStatus = courierStatus;
+    }
+
     private CourierType courierType;
     private CourierStatus courierStatus;
     public enum CourierType{
@@ -20,12 +77,12 @@ public class Courier {
         DELIVERED,
         CANCELED
     }
-    private Courier(String title,
-                    String senderName,
-                    String receiverName,
-                    CourierType courierType,
-                    int sourcePincode,
-                    int destinationPincode){
+    Courier(String title,
+            String senderName,
+            String receiverName,
+            CourierType courierType,
+            int sourcePincode,
+            int destinationPincode){
         this.title = title;
         this.senderName = senderName;
         this.receiverName = receiverName;
---
+++ src/FlipkartInterview/CourierDAO.java
@@ -0,0 +1,58 @@
+package FlipkartInterview;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CourierDAO implements CourierDAOInterface {
+List<Courier> courierList = new ArrayList<>();
+
+    @Override
+    public void createCourier(String title, String senderName, String receiverName, String courierType, int sourcePIN, int destinationPIN) {
+        courierList.add(new Courier(title, senderName, receiverName, Courier.CourierType.valueOf(courierType), sourcePIN, destinationPIN));
+    }
+
+    @Override
+    public void updateDeliveryStatus(String title, String courierStatus) {
+        courierList
+            .stream()
+            .filter(courier -> courier.getTitle().equals(title))
+            .findAny().get().setCourierStatus(Courier.CourierStatus.valueOf(courierStatus.toUpperCase()));
+    }
+
+    @Override
+    public void displayAllCouriers() {
+        courierList.forEach(System.out::println);
+    }
+
+    @Override
+    public void displayCouriersBySender(String senderName) {
+        courierList
+                .stream()
+                .filter(courier -> courier.getSenderName().equals(senderName))
+                .toList()
+                .forEach(System.out::println);
+    }
+
+    @Override
+    public void displayCourierByStatus(String courierStatus) {
+        courierList
+                .stream()
+                .filter(courier -> courier
+                        .getCourierStatus()
+                        .toString()
+                        .equals(courierStatus.toUpperCase()))
+                .forEach(System.out::println);
+    }
+
+    @Override
+    public void displayCourierByType(String courierType) {
+        courierList
+                .stream()
+                .filter(courier -> courier
+                        .getCourierType()
+                        .toString()
+                        .equals(courierType.toUpperCase()))
+                .forEach(System.out::println);
+    }
+}
