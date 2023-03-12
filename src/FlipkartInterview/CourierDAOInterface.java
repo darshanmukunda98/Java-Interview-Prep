cc3c92dc130578df7e273de82e1c79c821b69536
Darshan M <darshanmukunda098@gmail.com>
2023-03-12 11:55:51 +0530

    CourierDAO interface

---
+++ src/FlipkartInterview/CourierDAOInterface.java
@@ -0,0 +1,19 @@
+package FlipkartInterview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface CourierDAOInterface {
+    public void createCourier(String title,
+                              String senderName,
+                              String receiverName,
+                              String courierType,
+                              int sourcePIN,
+                              int destinationPIN);
+    public void updateDeliveryStatus(String title,
+                                     String courierStatus);
+    public void displayAllCouriers();
+    public void displayCouriersBySender(String senderName);
+    public void displayCourierByStatus(String courierStatus);
+    public void displayCourierByType(String courierType);
+}
